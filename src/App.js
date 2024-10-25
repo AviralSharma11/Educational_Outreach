5d633333ecaaba9b4fe85ecdac70f2e32b4f022b
NSuryansh <nagar.suryansh@gmail.com>
2024-10-26 00:41:06 +0530

    Devendra sir ad suhas sir images addition

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Header from './Components/Header';
+import Heroes from './Components/Heroes';
+import Director_message from './Components/Director_message';
+import Devandra_sir_message from './Components/Devendra_sir_message';
+import Cards from './Components/Cards';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Heroes />
+      <Director_message />
+      <Devandra_sir_message />
+      <Cards />
     </div>
   );
 }
