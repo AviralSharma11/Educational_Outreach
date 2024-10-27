fbccc6215cac301923ce6647c35946b12c439aa4
NSuryansh <nagar.suryansh@gmail.com>
2024-10-27 14:21:24 +0530

    list of MOU created

--- src/Components/Engagements.jsx
+++ src/Components/Engagements.jsx
@@ -1,5 +1,50 @@
 import React from 'react';
-import '../Styles/Engagements.css'
+import '../Styles/Engagements.css';
+
+const mouData = [
+  {
+    id: 1,
+    nature: 'MoU',
+    title: 'Madhya Pradesh State Skill Development and Employment Generation Board (MPSSDEGB)',
+    signedOn: '4/10/2023',
+    duration: '5 Years',
+  },
+  {
+    id: 2,
+    nature: 'MoU',
+    title: 'Sant Shiromani Ravidas Global Skill Park',
+    signedOn: '4/10/2023',
+    duration: '5 Years',
+  },
+  {
+    id: 3,
+    nature: 'MoU',
+    title: 'VECV (Research collaboration between IIT and VECV)',
+    signedOn: '31/07/2023',
+    duration: '5 Years',
+  },
+  {
+    id: 4,
+    nature: 'MoU',
+    title: 'VECV (Executive MTech program in Electric Vehicle Technology by IIT and VECV employees)',
+    signedOn: '31/07/2023',
+    duration: '5 Years',
+  },
+  {
+    id: 5,
+    nature: 'MoU',
+    title: 'Dept of Technical Education and Skill Development and Employment (Part Time Ph.D.)',
+    signedOn: '28/08/2023',
+    duration: '5 Years',
+  },
+  {
+    id: 6,
+    nature: 'MoU',
+    title: 'Dept of Technical Education and Skill Development and Employment (Inbound UG students)',
+    signedOn: '28/08/2023',
+    duration: '5 Years',
+  },
+];
 
 export default function Engagements() {
   return (
@@ -20,48 +65,15 @@ export default function Engagements() {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>1</td>
-                <td>MoU</td>
-                <td>Madhya Pradesh State Skill Development and Employment Generation Board (MPSSDEGB)</td>
-                <td>4/10/2023</td>
-                <td>5 Years</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>MoU</td>
-                <td>Sant Shiromani Ravidas Global Skill Park</td>
-                <td>4/10/2023</td>
-                <td>5 Years</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td>MoU</td>
-                <td>VECV (Research collaboration between IIT and VECV)</td>
-                <td>31/07/2023</td>
-                <td>5 Years</td>
-              </tr>
-              <tr>
-                <td>4</td>
-                <td>MoU</td>
-                <td>VECV (Executive MTech program in Electric Vehicle Technology by IIT and VECV employees)</td>
-                <td>31/07/2023</td>
-                <td>5 Years</td>
-              </tr>
-              <tr>
-                <td>5</td>
-                <td>MoU</td>
-                <td>Dept of Technical Education and Skill Development and Employment (Part Time Ph.D.)</td>
-                <td>28/08/2023</td>
-                <td>5 Years</td>
-              </tr>
-              <tr>
-                <td>6</td>
-                <td>MoU</td>
-                <td>Dept of Technical Education and Skill Development and Employment (Inbound UG students)</td>
-                <td>28/08/2023</td>
-                <td>5 Years</td>
-              </tr>
+              {mouData.map((mou) => (
+                <tr key={mou.id}>
+                  <td>{mou.id}</td>
+                  <td>{mou.nature}</td>
+                  <td>{mou.title}</td>
+                  <td>{mou.signedOn}</td>
+                  <td>{mou.duration}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
