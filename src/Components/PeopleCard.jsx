6fc324f1d412b6f90bb8e2e87473d265607c4c90
NSuryansh <nagar.suryansh@gmail.com>
2024-11-02 07:41:28 +0530

    Changes int Our People Page added heading for photod

--- src/Components/PeopleCard.jsx
+++ src/Components/PeopleCard.jsx
@@ -3,21 +3,21 @@ import "../Styles/OurPeople.css";
 
 export default function PeopleCard({ img, detail, position, contact, email, address }) {
     return (
-        <div className="people-card">
-            <div className="people-card-outer">
-                <div className="people-card-image">
-                    <img src={img} />
-                </div>
-                <div className="wrapping">
-                    <div className="Info " style={{ fontWeight: "600" }}>{detail}</div>
-                    <div className="Info " style={{ fontSize: "12px" }}>{position}</div>
-                </div>
-                <div className="people-card-detail">
-                    <div className="Info" style={{ fontWeight: "600" }}>{detail}</div>
-                    <div className="Info" style={{ fontSize: "10px" }}>{position}</div>
-                    <div className="Info" style={{ fontSize: "10px" }}>Contact No: {contact}</div>
-                    <div className="Info" style={{ fontSize: "10px" }}>{email}</div>
-                    <div className="Info" style={{ fontSize: "10px" }}>{address}</div>
+        <div className="people-card-wrapper">
+            <div className="people-card-heading">{position}</div> {/* Position heading outside the card box */}
+            <div className="people-card">
+                <div className="people-card-outer">
+                    <div className="people-card-image">
+                        <img src={img} alt="Person" />
+                        
+                    </div>
+                    <div className="Info people-card-deatil-text" style={{ fontWeight: "600" }}>{detail} </div>
+                    <div className="people-card-detail">
+                        <div className="Info" style={{ fontWeight: "600" }}>{detail} </div>
+                        <div className="Info" style={{ fontSize: "10px" }}>Contact No: {contact}</div>
+                        <div className="Info" style={{ fontSize: "10px" }}>{email}</div>
+                        <div className="Info" style={{ fontSize: "10px" }}>{address}</div>
+                    </div>
                 </div>
             </div>
         </div>
