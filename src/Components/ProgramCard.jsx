5f345ac2716b18ac39e29cdbeef5c821b0c244e8
NSuryansh <nagar.suryansh@gmail.com>
2024-11-04 17:07:04 +0530

    bug fixes

--- src/Components/ProgramCard.jsx
+++ src/Components/ProgramCard.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "../Styles/Program.css";
 
-export default function ProgramCard({img ,detail}){
-    return(
+export default function ProgramCard({ img, detail }) {
+    return (
         <div className="program-card">
-            <div className="program-card-image">
-                <img src = {img} />
-            </div>
-            <div className="program-card-detail">
-                    {detail}
+            <div className="program-card-outer">
+                <div className="program-card-front">
+                    <div className="program-card-image">
+                        <img src={img} alt="Program" />
+                    </div>
+                </div>
+                <div className="program-card-back">
+                    <div className="program-card-detail">
+                        {detail}
+                    </div>
+                </div>
             </div>
         </div>
     );
